bd9992aaebab6f0a6a526d071fc9f8c52c0ecb25
Ryan Roga <ryanroga@gmail.com>
2025-08-13 16:54:48 -0700

    Add MapContainer component to main page
    MapContainer is now exported from the components index and imported into the main page, replacing the default SvelteKit welcome content.

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -3,7 +3,7 @@
  */
 
 // Component exports will be added as components are implemented
-// export { default as MapContainer } from './MapContainer.svelte';
+export { default as MapContainer } from './MapContainer.svelte';
 // export { default as ProjectMarkers } from './ProjectMarkers.svelte';
 // export { default as ProjectModal } from './ProjectModal.svelte';
 // export { default as FilterControls } from './FilterControls.svelte';
