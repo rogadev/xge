7516d1dc9e296aada686f0d37de7a61e5311060d
Ryan Roga <ryanroga@gmail.com>
2025-08-13 16:24:31 -0700

    Stub out files for components and stores

---
+++ src/lib/stores/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Barrel export for all Svelte stores
+ */
+
+// Store exports will be added as stores are implemented
+// export * from './map.js';
+// export * from './projects.js';
+// export * from './selectedProject.js';
+// export * from './filters.js';
