008c59ee507d5c4bde4dbf25059706dc65c96c13
Ryan Roga <ryanroga@gmail.com>
2025-08-13 16:41:13 -0700

    Enable exports for stores module
    Uncomments and activates the export for the stores module in the library index file, making store functionality available for use.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -8,8 +8,8 @@ export * from './types/index.js';
 // Components (will be enabled as components are implemented)
 // export * from './components/index.js';
 
-// Stores (will be enabled as stores are implemented)
-// export * from './stores/index.js';
+// Stores
+export * from './stores/index.js';
 
 // Data
 export * from './data/index.js';
